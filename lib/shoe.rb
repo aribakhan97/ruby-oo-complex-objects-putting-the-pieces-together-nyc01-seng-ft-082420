a90ff7d5e1e01c3af51cf83635facc391b6127b0
aribakhan97 <akhan41@binghamton.edu>
2020-08-25 21:50:22 -0400

    created class book and class shoe

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,82 @@
-# Make your shoe class here!
+
+class Shoe
+
+    def initialize(brand)
+      @brand = brand
+    end 
+
+    def brand
+        @brand
+    end  
+
+    def color=(color)
+        @color= color
+    end 
+
+    def color
+        @color
+    end 
+
+    def size=(num)
+        @size = num
+    end 
+
+    def size
+        @size
+    end 
+
+    def material=(suede)
+        @suede = suede
+    end 
+
+    def material
+        @suede
+    end 
+
+    def condition=(condition)
+        @condition = condition
+    end 
+
+    def condition
+        @condition 
+    end 
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end 
+    
+end 
+
+#     def title 
+#         @title
+#     end 
+
+#     def author=(author)
+#         @author = author 
+#     end 
+#     def author
+#         @author
+#     end 
+    
+#     def page_count=(num)
+#         @page_count = num
+#     end
+
+#     def page_count
+#         @page_count
+#     end 
+
+#     def genre=(genre)
+#         @genre = genre
+#     end 
+
+#     def genre
+#         @genre
+#     end 
+
+#     def turn_page
+#         puts "Flipping the page...wow, you read fast!"
+#     end 
+
+# end 
